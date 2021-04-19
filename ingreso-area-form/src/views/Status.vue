41c893d21ff35dc8c1d6c0ae280ebf1601c86086
c.rojas.santiago@gmail.com <c.rojas.santiago@gmail.com>
2021-04-19 19:27:23 -0400

    retrieve info from id

---
+++ ingreso-area-form/src/views/Status.vue
@@ -0,0 +1,125 @@
+<template>
+  <div id="app">
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+    >
+    <section class="permiso">
+    <div class="admin">  
+    <md-card  class="md-layout-item md-size-100 md-small-size-100 box">        
+      <!--El header contiene el nombre del formulario y el logo de la empresa-->
+      <md-card-header >        
+        <!--Contenido del header-->
+      <b-container fluid>
+        <b-row class="md-title">
+          <img
+          class="responsive-img"
+          width="300px"
+          height="300px"
+          src="../assets/mlp-logo2.png"
+          />
+        </b-row>
+      </b-container>
+      </md-card-header>
+      <md-card-content>
+        <md-card class="md-layout-item md-size-50 md-small-size-100 box">
+        <md-card-header>
+          <div class="md-title">Estado de permiso</div>
+        </md-card-header>  
+        </md-card>
+      </md-card-content>
+      <md-card-content>
+        <div class="md-layout md-gutter">
+          <div class="md-layout-item md-small-size-100">
+            <md-field>
+              <label >Documento de ID {{IDkey}}</label>
+            </md-field>
+          </div>
+        </div>
+      </md-card-content>
+    </md-card>
+    </div> 
+    </section> 
+    <Footer/>
+  </div>  
+</template>
+
+<script>
+import Footer from '@/components/Footer.vue';
+import db, {Firebase} from '@/db'
+
+
+export default {
+  name: 'App',
+  components: {
+    Footer
+  },
+  data(){
+    return {
+      IDkey:this.$route.params.id,
+    }
+  },
+  mounted() {
+    const ref = this 
+    const db_r = db.collection('registers').doc(this.$route.params.id);
+    (async () => {
+    const doc = await db_r.get();
+    if (!doc.exists) {
+      this.data_review = 'No existe'
+  console.log('No such document!');
+  } else {
+    this.data_review = doc.data()
+    console.log('Document data:', this.data_review);
+  }
+  })();  
+  },
+
+  computed:{
+    status: function () {
+      if(!this.data_review['status']) return 'no existe'
+      else{
+        if (this.data_review['status']=='0'){ return 'en espera aprobación supervisor MLP'}
+        else if (this.data_review['status']=='1'){ return 'en espera aprobación efe de turno'}
+        else if (this.data_review['status']=='2'){ return 'aprobado. Comprobante enviado a'}
+        else{
+          return 'rechazado. Comprobante enviado a:'
+        }
+      }
+    },
+  },
+
+  watch: {
+    data_review: {
+      // call it upon creation too
+      immediate: true,
+      handler(data_review) {
+        this.$bind('userData', db.collection('registers').doc(this.$route.params.id || '1'))
+      },
+    },
+  },
+}
+</script>
+
+<style>
+#app {
+  background-image: linear-gradient(
+    to top,
+    rgb(51, 121, 147) 0%,
+    rgb(171, 214, 216) 100%
+  );
+  /* font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px; */
+}
+.md-menu-content.md-select-menu {
+  width: auto;
+  /*max-width: 400px !important;*/
+}
+.md-title {
+  font-size: 20px !important;
+}
+
+</style>
